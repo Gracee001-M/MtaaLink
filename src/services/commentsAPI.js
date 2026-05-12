f7f63ac4c0fbd09c2630da90e2029013bc42caa5
wachira-54 <wambugu.joelwachira54@gmail.com>
2026-05-12 15:30:58 +0300

    fixing empty files by staging content

---
+++ src/services/commentsAPI.js
@@ -0,0 +1,12 @@
+export const commentsAPI = {
+    getByPost: (postId) => request(`/posts/${postId}/comments`),
+    
+    create: (postId, commentData) => request(`/posts/${postId}/comments`, {
+        method: 'POST',
+        body: JSON.stringify(commentData)
+    }),
+    
+    delete: (postId, commentId) => request(`/posts/${postId}/comments/${commentId}`, {
+        method: 'DELETE'
+    })
+};
---
+++ src/services/postAPI.js
@@ -0,0 +1,26 @@
+export const postsAPI = {
+    getAll: (params = {}) => {
+        const query = new URLSearchParams(params).toString();
+        return request(`/posts${query ? `?${query}` : ''}`);
+    },
+    
+    getById: (id) => request(`/posts/${id}`),
+    
+    create: (postData) => request('/posts', {
+        method: 'POST',
+        body: JSON.stringify(postData)
+    }),
+    
+    update: (id, postData) => request(`/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(postData)
+    }),
+    
+    delete: (id) => request(`/posts/${id}`, {
+        method: 'DELETE'
+    }),
+    
+    like: (id) => request(`/posts/${id}/like`, {
+        method: 'POST'
+    })
+};
